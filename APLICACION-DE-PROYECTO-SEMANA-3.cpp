c6895268294f291817331835c27e70d2a92fd26e
Jorge <jorgeramostech91@gmail.com>
2026-09-16 18:28:23 -0400

    aplicacion de conversor terminada

--- APLICACION-DE-PROYECTO-SEMANA-3.cpp
+++ APLICACION-DE-PROYECTO-SEMANA-3.cpp
@@ -1,30 +1,98 @@
 #include <iostream>
-#include <string>
 #include <stack>
+#include <string>
+#include <cctype>
 
 using namespace std;
 
-class ConversorPostfix {
-public:
-    int prioridad(char operador) {
-        if (operador == '+' || operador == '-')
-            return 1;
+int prioridad(char operador)
+{
+    if (operador == '+' || operador == '-')
+        return 1;
+    if (operador == '*' || operador == '/')
+        return 2;
+    if (operador == '^')
+        return 3;
+    return 0;
+}
 
-        if (operador == '*' || operador == '/')
-            return 2;
+bool esOperador(char caracter)
+{
+    return caracter == '+' || caracter == '-' ||
+           caracter == '*' || caracter == '/' ||
+           caracter == '^';
+}
 
-        return 0;
+int main()
+{
+    string infija, postfija;
+    stack<char> operadores;
+
+    cout << "Porfavor Ingrese la expresion infija que desea Convertir a postfija: ";
+    getline(cin, infija);
+
+    for (char caracter : infija)
+    {
+        // Ignorar espacios
+        if (isspace(caracter))
+        {
+            continue;
+        }
+
+        // Si es operando: letras o numeros
+        if (isalnum(caracter))
+        {
+            postfija += caracter;
+            postfija += ' ';
+        }
+        // Abrir parentesis
+        else if (caracter == '(')
+        {
+            operadores.push(caracter);
+        }
+        // Cerrar parentesis
+        else if (caracter == ')')
+        {
+            while (!operadores.empty() && operadores.top() != '(')
+            {
+                postfija += operadores.top();
+                postfija += ' ';
+                operadores.pop();
+            }
+
+            if (!operadores.empty())
+            {
+                operadores.pop(); // Elimina '('
+            }
+        }
+        // Operadores
+        else if (esOperador(caracter))
+        {
+            while (!operadores.empty() &&
+                   operadores.top() != '(' &&
+                   prioridad(operadores.top()) >= prioridad(caracter))
+            {
+                postfija += operadores.top();
+                postfija += ' ';
+                operadores.pop();
+            }
+
+            operadores.push(caracter);
+        }
     }
 
-    bool esOperador(char caracter) {
-        return caracter == '+' || caracter == '-' ||
-               caracter == '*' || caracter == '/';
+    // Vaciar los operadores pendientes
+    while (!operadores.empty())
+    {
+        if (operadores.top() != '(')
+        {
+            postfija += operadores.top();
+            postfija += ' ';
+        }
+        operadores.pop();
     }
-}; int main() {
-    ConversorPostfix conversor;
 
-    cout << "Prioridad de +: " << conversor.prioridad('+') << endl;
-    cout << "Prioridad de *: " << conversor.prioridad('*') << endl;
+    cout << "\nEl Resultado de la Expresion postfija es: " << postfija << endl;
 
     return 0;
 }
